0fb1a5fe4380a3060e612fbece825038d9bbaba4
Kris Zyp <kriszyp@gmail.com>
2010-01-15 10:59:27 -0700

    Added a module to help normalize for Node

---
+++ node-perstore.js
@@ -0,0 +1,27 @@
+/**
+ * This is a helper script for starting Node with the correct paths and 
+ * environment to run Perstore
+ */
+
+// first we add all the necessary paths to require.paths
+var packagesRoot = "../../../";
+var packagePaths = [""] // start with the current directory
+			.concat([ // now add alll the packages
+				"packages/perstore/",
+				"packages/perstore/engines/node/",
+				"packages/perstore/engines/default/",
+				"packages/commonjs-utils/",
+				"engines/default/",
+				""
+				].map(function(path){ // for each package, start in the right directory
+					return packagesRoot + path;
+				    }));
+				    
+require.paths.push.apply(require.paths, packagePaths.map(function(path){
+	return path + "lib";
+}));
+
+var sys = require("sys");
+// upgrade to ES5 and CommonJS globals 
+global = this;
+require("global");
