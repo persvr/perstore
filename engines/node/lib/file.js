b18bcd791ff6a8368775e4fc171c3e7d5c65e332
Kris Zyp <kriszyp@gmail.com>
2010-03-15 10:04:54 -0600

    Expanded file normalization

--- engines/node/lib/file.js
+++ engines/node/lib/file.js
@@ -1,3 +1,16 @@
 var fs = require("fs");
 exports.read = fs.readFileSync;
 exports.write = fs.writeFileSync;
+exports.isFile = function(path){
+	try{
+		return fs.statSync(path).isFile();
+	}catch(e){
+		return false;
+	}
+};
+exports.join = function () {
+    if (arguments.length == 1 && arguments[0] == "") {
+        return '/';
+    }
+    return Array.prototype.join.call(arguments, '/');
+};
