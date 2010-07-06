0f119fec8c7528e71061805f52c0d0b10209ac07
Kris Zyp <kriszyp@gmail.com>
2010-07-06 07:23:25 -0600

    Start mapping to patr and promised-io

--- tests/all-tests.js
+++ tests/all-tests.js
@@ -2,6 +2,6 @@ exports.testPersistence = require("./model");
 exports.testFacet = require("./facet");
 exports.testStores = require("./store/all-stores");
 
-if (true)
-    require("os").exit(require("test/runner").run(exports));
+if (require.main === module)
+    require("patr/runner").run(exports);
 
