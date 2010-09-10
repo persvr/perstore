f4faf5cff7bd0fa4c55c4da3dfc712bf9a302ccd
Kris Zyp <kriszyp@gmail.com>
2010-09-10 15:10:37 -0600

    Updated performance base test

--- tests/store/perf/base.js
+++ tests/store/perf/base.js
@@ -1,28 +1,18 @@
 var store;
 var testQuery = require("rql/parser").parse("foo=hi");
 exports.tests = {
-	testPut: {
-		runTest: function(){
-			store.put({id:1,foo:3});
-		},
-		iterations: 1000
+	iterations: 1000,
+	testPut: function(){
+		store.put({id:1,foo:3});
 	},
-	testGet: {
-		runTest: function(){
-			store.get(1);
-		},
-		iterations: 1000
+	testGet: function(){
+		store.get(1);
 	},
-	testQuery: {
-		runTest: function(){
-			store.query(testQuery);
-		},
-		iterations: 1000
-	}
-	
-	
+	testQuery: function(){
+		store.query(testQuery);
+	}	
 };
-exports.testStore = function(storeToTest){
+exports.testStore = function(storeToTest, args){
 	store = storeToTest;
-    require("patr/runner").run(exports.tests);
+    require("patr/runner").run(exports.tests, args);
 };
